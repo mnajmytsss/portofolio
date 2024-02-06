69799796a7be3cee06fdeb57ac3ce84c6927279f
mnajmytsss <najmytsaqiby22@gmail.com>
2024-02-06 10:23:40 +0700

    fix: path to navbar fromthe header components

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,8 +3,7 @@
 import { ImageField } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
 import clsx from "clsx";
-import { useEffect, useRef } from "react";
-import { gsap } from "gsap";
+import { useRef } from "react";
 
 type AvatarProps = {
   image: ImageField;
@@ -13,52 +12,6 @@ type AvatarProps = {
 
 export default function Avatar({ image, className }: AvatarProps) {
   const component = useRef(null);
-
-//   useEffect(() => {
-//     let ctx = gsap.context(() => {
-//       gsap.fromTo(
-//         ".avatar",
-//         { opacity: 0, scale: 1.4 },
-//         { scale: 1, opacity: 1, duration: 1.3, ease: "power3.inOut" }
-//       );
-
-//       window.onmousemove = (e) => {
-//         if (!component.current) return;
-//         const componentRect = (
-//           component.current as HTMLElement
-//         ).getBoundingClientRect();
-//         const componentCenterX = componentRect.left + componentRect.width / 2;
-
-//         let componentPercent = {
-//           x: (e.clientX - componentCenterX) / componentRect.width / 2,
-//         };
-
-//         let distFromCenter = 1 - Math.abs(componentPercent.x);
-
-//         gsap
-//           .timeline({
-//             defaults: { defaults: 0.5, overwrite: "auto", ease: "power3.out" },
-//           })
-//           .to(
-//             ".avatar",
-//             {
-//               rotation: gsap.utils.clamp(-2, 2, 5 * componentPercent.x),
-//               duration: 0.5,
-//             },
-//             0
-//           )
-//           .to(
-//             "highlight",
-//             {
-//               opacity: distFromCenter - 0.7,
-//               x: (-10 * 20) & componentPercent.x,
-//               duration: 0.5,
-//             },
-//             0
-//           );
-//       };
-//     }, component);
-//   });
 
   return (
     <div ref={component} className={clsx("relative h-full w-full", className)}>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { createClient } from "@/prismicio";
-import Link from "next/link";
-import { PrismicNextLink } from "@prismicio/next";
-import NavBar from "@/components/NavBar"
+import NavBar from "./NavBar"
 
 export default async function Header() {
   const client = createClient();
